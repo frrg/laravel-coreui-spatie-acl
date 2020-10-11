4c3ecb4d7a9d634bf54f0407ad9ebc4697397998
ferifahrul <ferifahrul1@gmail.com>
2020-10-11 15:21:17 +0700

    use uuid for id user

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Webpatser\Uuid\Uuid;
 
 class User extends Authenticatable
 {
@@ -33,7 +34,22 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public $incrementing  = false;
+    protected $casts = ['id' => 'string','email_verified_at' => 'datetime'];
+    protected $guard_name = 'web';
+
+    public function setPasswordAttribute($value)
+    {
+        if (!empty($value)) $this->attributes['password'] = bcrypt($value);
+    }
+
+    // UUID
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->id = (string) Uuid::generate(4);
+        });
+    }
+   
 }
